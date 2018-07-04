a1756e74aae1c43cbb1bc220635254923d835225
Ain Sal <ain@abcd.sa>
2018-07-04 19:38:32 +0300

    Support reporting in dev mode
    Closes #2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.5
+
+- Support reporting in dev mode. To replicate the old behavior you can simply do `initAnalytics(process.env.NODE_ENV === 'production' ? 'PRODUCTION_TRACKING_ID' : 'ANY_STRING')`.
+
 # 0.0.4
 
 - Expose `trackPage`.
--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,14 @@
 import ga from 'react-ga';
 
-const dev = process.env.NODE_ENV !== 'production';
-
 export const initAnalytics = (trackingId, opts = {}) =>
-  !dev && ga.initialize(trackingId, opts);
+  ga.initialize(trackingId, opts);
 
 export const trackPage = (page, opts = {}, trackerNames, title) => {
-  if (!dev) {
-    ga.set({ page, ...opts });
-    ga.pageview(page, trackerNames, title);
-  }
+  ga.set({ page, ...opts });
+  ga.pageview(page, trackerNames, title);
 };
 
-export const trackUser = (userId, opts = {}) =>
-  !dev && ga.set({ userId, ...opts });
+export const trackUser = (userId, opts = {}) => ga.set({ userId, ...opts });
 
 export const trackEvent = (category, action, label, opts = {}) =>
-  !dev && ga.event({ category, action, label, ...opts });
+  ga.event({ category, action, label, ...opts });
